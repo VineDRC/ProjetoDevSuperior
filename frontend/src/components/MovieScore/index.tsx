d5849edbafde86b8b9811594b9da5022afcae1bc
Vinicius Correa <viniciusdosreiscorrea@gmail.com>
2022-01-16 21:02:57 -0300

    score

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -1,16 +1,18 @@
 import MovieStars from "components/MovieStars";
 import './style.css';
 
-function MovieScore() {
-    let score = 3.5;
-    let count = 13;
+type Props = {
+    score: number;
+    count: number;
+}
+
+    function MovieScore({score, count} : Props) {
     return (
         <div className="dsmovie-score-container">
             <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : 'Sem avaliações'} </p>
-            <MovieStars />
+            <MovieStars score={score}/>
             <p className="dsmovie-score-count">{count} avaliações</p>
         </div>
     );
 }
-
 export default MovieScore;
